3765485739d880a2ebe8182ef4fb6ca3d5763fd5
softhelpvideos <113528109+micalopez0112@users.noreply.github.com>
2024-01-21 00:24:29 -0300

    delete contact form and full screen

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,18 +4,8 @@ import { CiMail } from "react-icons/ci";
 import { FaInstagram } from "react-icons/fa";
 
 const Contact = () => {
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
-
   const email = "micaelalopezm@gmail.com";
   const instagramUsername = "vacaciones_punta_rental";
-
-  const handleSendEmail = () => {
-    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(message)}`;
-    window.location.href = mailtoLink;
-  };
 
   return (
     <div className="contact-container">
@@ -30,20 +20,6 @@ const Contact = () => {
           target="_blank"
           rel="noopener noreferrer"
         >{`@${instagramUsername}`}</a>
-      </div>
-      <div className="contact-form">
-        <label>Asunto:</label>
-        <input
-          type="text"
-          value={subject}
-          onChange={(e) => setSubject(e.target.value)}
-        />
-        <label>Mensaje:</label>
-        <textarea
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button onClick={handleSendEmail}>Enviar correo</button>
       </div>
     </div>
   );
