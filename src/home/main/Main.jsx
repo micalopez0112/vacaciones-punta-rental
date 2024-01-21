3d1d6c21defaee41ec726f57aef2bca4a0441f5a
softhelpvideos <113528109+micalopez0112@users.noreply.github.com>
2024-01-21 00:51:22 -0300

    fix

--- src/home/main/Main.jsx
+++ src/home/main/Main.jsx
@@ -6,7 +6,14 @@ import { Link } from "react-router-dom";
 const Main = () => {
   return (
     <div className="main">
-      <video className="video" src={video} autoPlay loop muted></video>
+      <video
+        className="video"
+        src={video}
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
       <div className="overlay"></div>
       <div className="content">
         <h1>Descubre tu próxima casa fuera de casa!</h1>
