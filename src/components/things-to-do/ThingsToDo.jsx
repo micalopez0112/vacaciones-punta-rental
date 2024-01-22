1afc6a3b88e8a98512ffc48c0e039eee6b73c492
softhelpvideos <113528109+micalopez0112@users.noreply.github.com>
2024-01-22 14:06:54 -0300

    things to do section

--- src/components/things-to-do/ThingsToDo.jsx
+++ src/components/things-to-do/ThingsToDo.jsx
@@ -7,113 +7,98 @@ const ThingsToDo = () => {
   return (
     <div className="things-to-do">
       <div className="blank-space"></div>
-      <div className="image-container">
+      <div className="activities">
         <div className="image-puerto">
           <img className="puerto-img" src={puertoImg} alt="" />
         </div>
-      </div>
-      <div className="things-to-do-container">
-        <div className="restaurants">
-          <div className="things-to-do-title">Restaurantes</div>
-          <div className="things-to-do-description">
-            Explorá la gastronomía local!
+
+        <div className="things-to-do-container">
+          <div className="restaurants">
+            <div className="things-to-do-title">Restaurantes</div>
+            <div className="things-to-do-description">
+              Explorá la gastronomía local!
+            </div>
+            <div className="to-do-list-container">
+              <ul className="to-do-list">
+                <li className="to-do-container">
+                  <a
+                    href="https://www.instagram.com/chilloutrestouy"
+                    className="to-do-list"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    • Chillout Resto
+                  </a>
+                </li>
+                <li className="to-do-list-container">
+                  <a
+                    href="https://www.instagram.com/elmercadopde"
+                    className="to-do-list"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    • El Mercado
+                  </a>
+                </li>
+                <li className="to-do-list-container">
+                  <a
+                    href="https://www.instagram.com/zazupuerto/"
+                    className="to-do-list"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    • Zazú Puerto
+                  </a>
+                </li>
+                <li className="to-do-list-container">
+                  <a
+                    href="https://www.instagram.com/boxgarden.pde/"
+                    className="to-do-list"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    • Box Garden
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="to-do-list-container">
-            <ul className="to-do-list">
-              <li className="to-do-container">
-                <a
-                  href="https://www.instagram.com/chilloutrestouy"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Chillout Resto
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/elmercadopde"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  El Mercado
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/zazupuerto/"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Zazú Puerto
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/boxgarden.pde/"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Box Garden
-                </a>
-              </li>
-            </ul>
+
+          <div className="places-to-visit">
+            <div className="things-to-do-title">Lugares para visitar</div>
+            <div className="things-to-do-description">
+              Recorré como un local!
+            </div>
+            <div className="to-do-list-container">
+              <ul className="to-do-list">
+                <li className="to-do-list-container">
+                  <a
+                    href="https://macamuseo.org/"
+                    className="to-do-list"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    • Museo de Arte Contemporáneo Atchugarry (MACA)
+                  </a>
+                </li>
+                <li className="to-do-list-container">
+                  <p className="to-do-list">• La Barra</p>
+                </li>
+                <li className="to-do-list-container">
+                  <p className="to-do-list">• El Puerto</p>
+                </li>
+                <li className="to-do-list-container">
+                  <p className="to-do-list">• Laguna Garzón</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-
-        <div className="places-to-visit">
-          <div className="things-to-do-title">Lugares para visitar</div>
-          <div className="things-to-do-description">Recorré como un local!</div>
-          <div className="to-do-list-container">
-            <ul className="to-do-list">
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/chilloutrestouy"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Museo de Arte Contemporáneo Atchugarry (MACA)
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/elmercadopde"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  La Barra
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href="https://www.instagram.com/zazupuerto/"
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  El Puerto
-                </a>
-              </li>
-              <li className="to-do-list-container">
-                <a
-                  href=""
-                  className="to-do-list"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Laguna Garzón
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
         <div className="more-things">
+          <div className="things-to-do-title">Más cosas para hacer</div>
+          <div className="things-to-do-description">
+            Si todavia no encontraste que hacer, vistá
+          </div>
           <a
             href="https://quehacemoshoy.com.uy/"
             target="_blank"
